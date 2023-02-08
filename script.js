f2cebaeab170b6ab363495e07a78b5cc3d1724af
BySTBN <estebanu.1012@gmail.com>
2023-02-08 09:09:07 -0500

    Nothing to commit

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-function encriptarTexto (){
-    var textoIngresado = document.querySelector("#text-area").value;
-    var textoEncriptado = textoIngresado.replace(/a/gi,"ai").replace(/e/gi, "enter").replace(/i/gi,"imes").replace(/o/gi,"ober").replace(/u/gi, "ufat");
-    document.querySelector("#mensaje-encriptado").value = textoEncriptado;
-    document.querySelector("#text-area").value;
+function encriptar() {
+    var texto = document.querySelector(".text-area").value;
+    textoEncriptado = texto.replace(/a/g, 'ai')
+    .replace(/e/g, 'enter')
+    .replace(/i/g, 'imes')
+    .replace(/o/g, 'ober')
+    .replace(/u/g, 'ufat');
+    document.querySelector(".mensaje-encriptado").value = textoEncriptado;
+    document.querySelector(".text-area");
+    alert("mensaje encriptado");
 }
 
-var boton1 = document.querySelector("btn-encriptar");
-boton1.onclick = encriptarTexto;
+var botonEncriptar = document.querySelector(".btn-encriptar");
+botonEncriptar.onclick = encriptar;
+
+function desencriptar(){
+    var texto = document.querySelector(".text-area").value;
+    textoEncriptado = texto.replace(/ai/g, 'a')
+    .replace(/enter/g, 'e')
+    .replace(/imes/g, 'i')
+    .replace(/ober/g, 'o')
+    .replace(/ufat/g, 'u');
+    document.querySelector(".mensaje-encriptado").value = textoEncriptado;
+    document.querySelector(".text-area");
+    alert("mensaje desencriptado");
+}
+
+var botonDesencriptar = document.querySelector(".btn-desencriptar");
+botonDesencriptar.onclick = desencriptar;
+
+function copiar(){
+    var  texto = document.querySelector(".mensaje-encriptado").value;
+    mensajeCopiado = texto;
+    navigator.clipboard.writeText(mensajeCopiado);
+    alert("mensaje copiado");
+}
+
+var botonCopiar = document.querySelector(".copiar");
+botonCopiar.onclick = copiar;
